25a8daf8cede47bcf59d718ca2b558bd19308fe6
wlasitz-jeremy <wlasitz@gmail.com>
2026-01-21 18:42:09 -0700

    test

--- Lab 1.py
+++ Lab 1.py
@@ -5,3 +5,4 @@ prov_code = str(input('Enter Province Code:'))
 post_code = str(input('Enter Postal Code:'))
 cust_name = str(input('Enter Customer  Name:'))
 print("Ship to:" + '\n' + cust_name + '\n' + str(civ_num) + ' ' + st_name + '\n' + cty_name + ',' + ' ' + prov_code + ' ' + post_code)
+
