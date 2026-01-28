fc048a33838f32635a11ea6d98fa0c39ed77e774
wlasitz-jeremy <wlasitz@gmail.com>
2026-01-28 13:36:23 -0700

    initial commit

---
+++ Lab 2.py
@@ -0,0 +1,11 @@
+age = int(input('Enter Age:'))
+yrs_service = int(input('Enter years of service:'))
+print('Age: '+str(age))
+print('Years of Service: '+str(yrs_service))
+if age>50:
+    if age>=65 or yrs_service+age>=80:
+        print(f'You are eligible for retirement with full pension benefits')
+    else:
+        print(f'You are eligible for retirement with discounted pension benifits')
+else:
+    print(f'You are not eligible for retirement')
