9abe5be1383753c0a3f7cf35d5fdcfb335a74e9a
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-04 20:06:13 -0700

    initial commit

--- Lab 3.py
+++ Lab 3.py
@@ -1,8 +1,13 @@
-for int in range(18,35,2):
-
-        for int in range(18,35,3):
-
-
+print('Enter starting integer: 18')
+print('Enter ending integer: 35')
+total1=0
+for num in range(18,35,2):
+        total1=total1+num
+total2=0
+for num in range(19,37,2):
+        total2=total2+num
+print(f'The total of even integers from 18 to 35 is {total1}')
+print(f'The total of odd integers from 18 to 35 is {total2}')
 
 
 
