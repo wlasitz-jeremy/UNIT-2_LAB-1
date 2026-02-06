0c331e6e3b7268eff54475796da6a88f6fa17d3c
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-06 15:08:47 -0700

    initial commit

---
+++ Assignment 1.py
@@ -0,0 +1,20 @@
+print('--------------------------------------------------')
+print('*** Welcome to the Berverage Wholesale Program ***')
+print('--------------------------------------------------')
+print('Please select type of purchace:')
+print('C: Coffee Beans')
+print('T: Tea Boxes')
+input('>>> ')
+if ch in input:
+    pass
+else:
+    print('Invalid input, you should enter c/C or t/T')
+if input == 'C' or 'c':
+    input('Enter the number of kilograms (kg) of coffee: ')
+else:
+    input('Enter the number of tea boxes: ')
+
+
+
+
+
