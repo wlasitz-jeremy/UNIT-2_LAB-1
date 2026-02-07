0e75c90b80ab1f313308e4ee1ac83a0c931ce80e
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-06 17:17:06 -0700

    initial commit

--- Assignment 1.py
+++ Assignment 1.py
@@ -9,20 +9,24 @@ coffeeammount=0
 teaboxammounnt=0
 if selection=='c' or selection=='C' or selection=='t' or selection=='T':
     if selection=='C' or selection=='c':
-        input('Enter the number of kilograms (kg) of coffee: ')
+        coffeeammount=input('Enter the number of kilograms (kg) of coffee: ')
+        if int(coffeeammount)<=0:
+            print('Quantity of coffee should be > 0')
+        else:
+            print(input('Please enter the 2-letter provine abbreviation: '))
     elif selection=='T' or selection=='t':
-        input('Enter the number of tea boxes: ')
+        teaboxammount=input('Enter the number of tea boxes: ')
+        if int(teaboxammounnt)<=0:
+            print('Quantity of tea boxes should be > 0')
+        else:
+            print(input('Please enter the 2-letter provine abbreviation: '))
 else:
     print('Invalid input, you should enter c/C or t/T')
 
-# if coffeeammount>0:
-#     print()
-# if teaboxammounnt>0:
-#
-#
-#
-# coffee=input('Quantity of coffee should be > ')
-# tea=input('Number of tea boxes should be > ')
+
+
+
+
 
 
 
