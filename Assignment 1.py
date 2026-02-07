da7b53f0ab0a16ab207617e87481b0f8a6ad0bb5
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-07 08:29:12 -0700

    finished coding through province selection and now making table format

--- Assignment 1.py
+++ Assignment 1.py
@@ -28,15 +28,16 @@ price_coffee=float(coffeeamount)*18.50
 price_tea=float(teaboxamount)*(20*0.45)
 if int(coffeeamount)>25 or int(teaboxamount)>10:
         if coffeeamount>25:
-           price_coffee=price_coffee*0.90
+           discount_price_coffee=price_coffee*0.90
         elif teaboxamount>10:
-            price_tea=price_tea*0.85
+            discount_price_tea=price_tea*0.85
 else:
-    discount_coffee=price_coffee*1
-    discount_tea=price_tea*1
+    discount_price_coffee=price_coffee*1
+    discount_price_tea=price_tea*1
+gst=0
 if province_abbreviation=='ab' or province_abbreviation=='Ab' or province_abbreviation=='AB' or province_abbreviation=='bc' or province_abbreviation=='Bc' or province_abbreviation=='BC' or province_abbreviation=='mb' or province_abbreviation=='Mb' or province_abbreviation=='MB' or province_abbreviation=='nb' or province_abbreviation=='Nb' or province_abbreviation=='NB' or province_abbreviation=='nl' or province_abbreviation=='Nl' or province_abbreviation=='NL' or province_abbreviation=='ns' or province_abbreviation=='Ns' or province_abbreviation=='NS' or province_abbreviation=='on' or province_abbreviation=='On' or province_abbreviation=='ON' or province_abbreviation=='pe' or province_abbreviation=='Pe' or province_abbreviation=='PE' or province_abbreviation=='qc' or province_abbreviation=='Qc' or province_abbreviation=='QC' or province_abbreviation=='sk' or province_abbreviation=='Sk' or province_abbreviation=='SK' or province_abbreviation=='nt' or province_abbreviation=='Nt' or province_abbreviation=='NT' or province_abbreviation=='nu' or province_abbreviation=='Nu' or province_abbreviation=='NU' or province_abbreviation=='yt' or province_abbreviation=='Yt' or province_abbreviation=='YT':
     if province_abbreviation=='ab' or province_abbreviation=='Ab' or province_abbreviation=='AB' or province_abbreviation=='bc' or province_abbreviation=='Bc' or province_abbreviation=='BC':
-        price_coffee*1.05 or price_tea*1.05
+        gst=price_coffee*1.05 or gst=price_tea*1.05
     if province_abbreviation=='on' or province_abbreviation=='On' or province_abbreviation=='ON':
         price_coffee*1.13 or price_tea*1.13
 
