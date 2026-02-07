f5a579ca731d3bda6480a332695f0fdf0996af77
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-06 17:23:02 -0700

    initial commit

--- Assignment 1.py
+++ Assignment 1.py
@@ -5,18 +5,16 @@ print('Please select type of purchace:')
 print('C: Coffee Beans')
 print('T: Tea Boxes')
 selection = input('>>> ')
-coffeeammount=0
-teaboxammounnt=0
 if selection=='c' or selection=='C' or selection=='t' or selection=='T':
     if selection=='C' or selection=='c':
-        coffeeammount=input('Enter the number of kilograms (kg) of coffee: ')
-        if int(coffeeammount)<=0:
+        coffeeamount=input('Enter the number of kilograms (kg) of coffee: ')
+        if int(coffeeamount)<=0:
             print('Quantity of coffee should be > 0')
         else:
             print(input('Please enter the 2-letter provine abbreviation: '))
     elif selection=='T' or selection=='t':
-        teaboxammount=input('Enter the number of tea boxes: ')
-        if int(teaboxammounnt)<=0:
+        teaboxamount=input('Enter the number of tea boxes: ')
+        if int(teaboxamount)<=0:
             print('Quantity of tea boxes should be > 0')
         else:
             print(input('Please enter the 2-letter provine abbreviation: '))
