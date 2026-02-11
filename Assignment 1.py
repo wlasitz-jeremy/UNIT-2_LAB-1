ae90e69a64042f6e89dd8d289b642e096d23871b
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-11 13:38:29 -0700

    initial commit

--- Assignment 1.py
+++ Assignment 1.py
@@ -7,52 +7,52 @@ print('T: Tea Boxes')
 selection=input('>>> ')
 coffee_amount=0
 tea_box_amount=0
+discount_price=0
 province_abbreviation=0
-discount_price_coffee=0
-discount_price_tea=0
+gst=0
+province_abbreviation = str(province_abbreviation).upper()
 if selection=='c' or selection=='C' or selection=='t' or selection=='T':
     if selection=='C' or selection=='c':
         coffee_amount=float(input('Enter the number of kilograms (kg) of coffee: '))
-        if int(coffee_amount)<=0:
+        if float(coffee_amount)<=0:
             print('Quantity of coffee should be > 0')
-            exit(1)
-        else:
-            province_abbreviation=(input('Please enter the 2-letter province abbreviation: '))
-    elif selection=='T' or selection=='t':
-        tea_box_amount=float(input('Enter the number of tea boxes: '))*20
-        if int(tea_box_amount)<=0:
-            print('Quantity of tea boxes should be > 0')
-            exit(1)
+            exit()
         else:
             province_abbreviation=input('Please enter the 2-letter province abbreviation: ')
+            price = float(coffee_amount) * 18.50
+            if coffee_amount > 25:
+                discount_price=float(price) * 0.90
+            else:
+                discount_price=price
+    elif selection=='T' or selection=='t':
+        tea_box_amount=float(input('Enter the number of tea boxes: '))
+        if float(tea_box_amount)<=0:
+            print('Quantity of tea boxes should be > 0')
+            exit()
+        else:
+            province_abbreviation=input('Please enter the 2-letter province abbreviation: ')
+            price=float(tea_box_amount) * 9
+            if tea_box_amount>10:
+                discount_price = float(price) * 0.85
+            else:
+                discount_price=price
 else:
     print('Invalid input, you should enter c/C or t/T')
-price_coffee=float(coffee_amount)*18.50
-price_tea=float(tea_box_amount)*0.45
-gst=0
-if coffee_amount>25 or tea_box_amount>10:
-    if coffee_amount>25:
-        discount_price_coffee=float(price_coffee)*0.90
-    elif tea_box_amount>10:
-        discount_price_tea=float(price_tea)*0.85
-else:
-    discount_price_coffee=price_coffee*1
-    discount_price_tea=price_tea*1
-if province_abbreviation=='ab' or province_abbreviation=='Ab' or province_abbreviation=='AB' or province_abbreviation=='bc' or province_abbreviation=='Bc' or province_abbreviation=='BC' or province_abbreviation=='mb' or province_abbreviation=='Mb' or province_abbreviation=='MB' or province_abbreviation=='nb' or province_abbreviation=='Nb' or province_abbreviation=='NB' or province_abbreviation=='nl' or province_abbreviation=='Nl' or province_abbreviation=='NL' or province_abbreviation=='ns' or province_abbreviation=='Ns' or province_abbreviation=='NS' or province_abbreviation=='on' or province_abbreviation=='On' or province_abbreviation=='ON' or province_abbreviation=='pe' or province_abbreviation=='Pe' or province_abbreviation=='PE' or province_abbreviation=='qc' or province_abbreviation=='Qc' or province_abbreviation=='QC' or province_abbreviation=='sk' or province_abbreviation=='Sk' or province_abbreviation=='SK' or province_abbreviation=='nt' or province_abbreviation=='Nt' or province_abbreviation=='NT' or province_abbreviation=='nu' or province_abbreviation=='Nu' or province_abbreviation=='NU' or province_abbreviation=='yt' or province_abbreviation=='Yt' or province_abbreviation=='YT':
-    if province_abbreviation=='ab' or province_abbreviation=='Ab' or province_abbreviation=='AB' or province_abbreviation=='bc' or province_abbreviation=='Bc' or province_abbreviation=='BC':
-        gst == float(discount_price_coffee) * 0.05 or gst == float(discount_price_tea) * 0.05
-    elif province_abbreviation=='on' or province_abbreviation=='On' or province_abbreviation=='ON':
-        gst == float(discount_price_coffee) * 0.13 or gst == float(discount_price_tea) * 0.13
+    exit()
+if province_abbreviation=='ab' or province_abbreviation=='bc' or province_abbreviation=='mb' or province_abbreviation=='nb' or province_abbreviation=='nl' or province_abbreviation=='ns' or province_abbreviation=='on' or province_abbreviation=='pe' or province_abbreviation=='qc' or province_abbreviation=='sk' or province_abbreviation=='nt' or province_abbreviation=='nu' or province_abbreviation=='yt':
+    if province_abbreviation=='ab' or province_abbreviation=='bc':
+        gst=discount_price * 0.05
+    elif province_abbreviation=='on':
+        gst=discount_price * 0.13
     else:
-        gst=discount_price_coffee*0.15 or gst==discount_price_tea*0.15
+        gst=discount_price*0.15
 else:
     print('Invalid input, you should enter correct Province Abbreviation')
     exit()
-total_price_coffee=discount_price_coffee + gst
-total_price_tea=discount_price_tea + gst
+total_price=discount_price + gst
 if selection=='c' or selection=='C':
-    print(f'{"Product":>10}{"Qty(Bags/kg)":>10}{"Price before Disc":>10}{"Price after Disc":>10}{"GST":>10}{"Total Price":>10}')
-    print(f'{"Coffee":>10}{coffee_amount:>10.2f}{price_coffee:>10.2f}{discount_price_coffee:>10.2f}{gst:>10.2f}{total_price_coffee:>10.2f}')
+    print(f'{"Product"}{"Qty(Bags/kg)":>15}{"Price before Disc":>20}{"Price after Disc":>20}{"GST":>20}{"Total Price":>20}')
+    print(f'{"Coffee":>5}{coffee_amount:>14.2f}{price:>10.2f}{discount_price:>10.2f}{gst:>10.2f}{total_price:>10.2f}')
 else:
-    print(f'{"Product":>10}{"Qty(Bags/kg)":>10}{"Price before Disc":>10}{"Price after Disc":>10}{"GST":>10}{"Total Price":>10}')
-    print(f'{"Tea":>10}{tea_box_amount:>10.2f}{price_tea:>10.2f}{discount_price_tea:>10.2f}{gst:>10.2f}{total_price_tea:>10.2f}')
+    print(f'{"Product"}{"Qty(Bags/kg)":>15}{"Price before Disc":>20}{"Price after Disc":>20}{"GST":>20}{"Total Price":>20}')
+    print(f'{"Tea":>5}{tea_box_amount:>14.2f}{price:>10.2f}{discount_price:>10.2f}{gst:>10.2f}{total_price:>10.2f}')
---
+++ Lab 4.py
@@ -0,0 +1,4 @@
+st=('Tiffany','Kyle','Celeste','John')
+sc=('55','80.13','96','64.88')
+
+print(f'Student name: ')
