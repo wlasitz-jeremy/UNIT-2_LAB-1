7250b1d0c96e507bae6e06a4f27c7b4970356b85
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-15 22:34:33 -0700

    finished assignment

--- Lab 4.py
+++ Lab 4.py
@@ -1,9 +1,9 @@
-
-
-
-
-
-
-
-
-
+names=[]
+score=[]
+while names != 'END':
+      names = input('Enter your name("END" to stop input): ')
+      score = input('Enter your score: ')
+stn= dict(names)
+stsc= dict(score)
+print(f'{"Student Name"} {"Grade":>21}\n'
+      f'{"-"*15}{"-"*5:>21}')
