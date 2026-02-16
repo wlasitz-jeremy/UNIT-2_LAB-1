a2cbc455efc82bf7a2cffe328241476556299140
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-15 23:15:10 -0700

    finished assignment

--- Lab 4.py
+++ Lab 4.py
@@ -3,7 +3,7 @@ score=[]
 while names != 'END':
       names = input('Enter your name("END" to stop input): ')
       score = input('Enter your score: ')
-stn= dict(names)
-stsc= dict(score)
-print(f'{"Student Name"} {"Grade":>21}\n'
-      f'{"-"*15}{"-"*5:>21}')
+print(f'Class average score is {sum(score)/len(score):.f}\n'
+      f'Highest score is {max(score):.f}\n'
+      f'{"Student Name"} {"Grade":>21}\n'
+      f'{"-"*15}{"-"*5:>21}\n')
