9e6ac899f0f97372aac65b328ff5fd22e799a5a8
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-17 16:25:46 -0700

    finished assignment

--- Lab 4.py
+++ Lab 4.py
@@ -5,8 +5,8 @@ while names != 'END':
       names = input('Enter your name("END" to stop input): ')
       score = input('Enter your score: ')
       for names, score in studentgrades:
-            studentgrades.update({names: score})
-print(f'Class average score is {sum(studentgrades.values)/len(studentgrades.values):.f}\n'
+            studentgrades.update({names:score})
+print(f'Class average score is {sum(studentgrades.values())/len(studentgrades.values()):.f}\n'
       f'Highest score is {max(studentgrades.values()):.f} achieved by {studentgrades.key()}\n'
       f'{"Student Name"} {"Grade":>21}\n'
       f'{"-"*15}{"-"*5:>21}\n'
