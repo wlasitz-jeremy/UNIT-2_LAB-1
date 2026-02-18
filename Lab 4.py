e866c32a86d2aeb5d65fa58676d8530a40e39e23
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-18 10:16:58 -0700

    still not working

--- Lab 4.py
+++ Lab 4.py
@@ -12,7 +12,7 @@ highest_name = max(students, key=students.get)
 class_average = sum(students.values())/len(students)
 print(f'Class average score is {class_average:.1f}\n'
       f'Highest score is {highest_score:.1f} achieved by {highest_name}\n'
-      f'{"Student Name"} {"Grade":>21}\n'
-      f'{"-"*15}{"-"*5:>21}\n')
+      f'{"Student Name"} {"Grade":>8}\n'
+      f'{"-"*15}{"-"*5:>6}\n')
 for name, score in students.items():
-      print(f'{name}{score:>21}\n')
+      print(f'{name}{score:>14.1f}\n')
