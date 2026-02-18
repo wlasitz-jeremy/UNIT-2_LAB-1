236bc191810bf081fcb5573064701a20589461d0
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-18 10:20:03 -0700

    still not working

--- Lab 4.py
+++ Lab 4.py
@@ -1,12 +1,10 @@
 students = {}
 name = []
 score = []
-while True:
+while name != 'END':
       name = input('Enter your name("END" to stop input): ')
-      if name == 'END':
-            break
-      score = float(input('Enter your score: '))
-      students[name]=score
+score = float(input('Enter your score: '))
+students[name]=score
 highest_score = students[max(students)]
 highest_name = max(students, key=students.get)
 class_average = sum(students.values())/len(students)
@@ -15,4 +13,4 @@ print(f'Class average score is {class_average:.1f}\n'
       f'{"Student Name"} {"Grade":>8}\n'
       f'{"-"*15}{"-"*5:>6}\n')
 for name, score in students.items():
-      print(f'{name}{score:>14.1f}\n')
+      print(f'{name}{score:>14}\n')
