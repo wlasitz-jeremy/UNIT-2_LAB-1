4e28e91ed1d99adbf4728905cf67668f7bf60096
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-25 13:11:05 -0700

    initial commit

--- Test.py
+++ Test.py
@@ -1,22 +1,34 @@
-def show_welcome():
-    """ Function to show welcome message """
-    print("Hello to the library!")
-def display_books():
-    """ Function to display books """
-    print("Here are our books...")
-def issue_books():
-    """ Function to issue books """
-    print("This book is for you!")
-def return_books():
-    """ Function to return books """
-    print("Thank-you for returning the books!")
-print(f"{show_welcome.__doc__}\n"
-      f"{display_books.__doc__}\n"
-      f"{issue_books.__doc__}\n"
-      f"{return_books.__doc__}\n")
-show_welcome()
-display_books()
-issue_books()
-return_books()
+# def show_welcome():
+#     """ Function to show welcome message """
+#     print("Hello to the library!")
+# def display_books():
+#     """ Function to display books """
+#     print("Here are our books...")
+# def issue_books():
+#     """ Function to issue books """
+#     print("This book is for you!")
+# def return_books():
+#     """ Function to return books """
+#     print("Thank-you for returning the books!")
+# print(f"{show_welcome.__doc__}\n"
+#       f"{display_books.__doc__}\n"
+#       f"{issue_books.__doc__}\n"
+#       f"{return_books.__doc__}\n")
+# show_welcome()
+# display_books()
+# issue_books()
+# return_books()
+
+
+# def total(value1,value2):
+#     total=0
+#     '''sum of numbers'''
+#     total=int(value1)+int(value2)
+#     print(f"The sum of the numbers is: {total}")
+#
+# value1=input("Enter first number: ")
+# value2=input("Enter second number: ")
+# print(total.__doc__)
+# total(value1,value2)
 
 
