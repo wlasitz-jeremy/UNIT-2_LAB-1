8ced5d7d5dd51b2d9648ad289c17ed06135aa14d
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-25 11:45:54 -0700

    initial commit

--- Lab 1 Test.py
+++
@@ -1,7 +0,0 @@
-civ_num = int(input('Enter Civic Number:'))
-st_name = str(input('Enter Street Name:'))
-cty_name = str(input('Enter City Name:'))
-prov_code = str(input('Enter Province Code:'))
-post_code = str(input('Enter Postal Code:'))
-cust_name = str(input('Enter Customer  Name:'))
-print("Ship to:" + '\n' + cust_name + '\n' + str(civ_num) + ' ' + st_name + '\n' + cty_name + ',' + ' ' + prov_code + ' ' + post_code)
