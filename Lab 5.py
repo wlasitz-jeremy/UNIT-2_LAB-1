a7b465663e4e5d31ff6dedd07336f7a3fbb723f2
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-25 11:48:59 -0700

    initial commit

---
+++ Lab 5.py
@@ -0,0 +1,10 @@
+import random
+NUM_DICE_TO_ROLL = 5
+SEED = 2183
+# implement roll_dice() here!
+# implement most_repeats() here!
+def main():
+    random.seed(SEED)
+# implement remaining logic here!
+if __name__ == "__main__":
+    main()
