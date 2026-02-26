c279e94de1c7897e2c52927a9a14f6ad17a6e2b8
wlasitz-jeremy <wlasitz@gmail.com>
2026-02-25 22:16:33 -0700

    finished

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,19 +1,26 @@
 print(f"Welcome to Circle Phones' Profit Calculator.")
-product_number = [0,1,2,3,4,5]
-quantity_sold = 0
-total_profit = 0
-category = {'applephone', 120.45,
-     'androidphone', 99.50,
-     'appletablet', 75.69,
-     'androidtablet', 65.73,
-     'windowstablet', 51.49}
-while product_number !='0':
-    product_number = input('Enter product number 1-5, or enter 0 to stop:\n       ')
-    quantity_sold = float(input('Enter quantity sold:\n       '))
-    if product_number == '0':
+total_profit = 0.0
+category = {
+    1: ('applephone', 120.45,),
+    2: ('androidphone', 99.50,),
+    3: ('appletablet', 75.69,),
+    4: ('androidtablet', 65.73,),
+    5: ('windowstablet', 51.49)
+    }
+while True:
+    product_number = input('Enter product number 1-5, or enter 0 to stop:\n       ').strip()
+    if product_number =='0':
         break
-if product_number==product_number:
-    total_profit = product_number * quantity_sold
-    print(f"Your total profit for today is {total_profit:.2f}")
-else:
-    print(f"Invalid input, please enter a valid number")
+    if product_number in category:
+        continue
+    else:
+        print('Invalid input, please enter a valid number')
+    product_number = int(product_number)
+    quantity_sold = input('Enter quantity sold:\n       ').strip()
+    if not quantity_sold.isdigit():
+        print('Invalid input, please enter 0 or more')
+        continue
+    quantity_sold = int(quantity_sold)
+    if quantity_sold < 0:
+        print('Quantity sold must be greater than 0')
+        continue
