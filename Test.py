c547852ed9d6d876919f2c378995e9a1f2b59622
wlasitz-jeremy <wlasitz@gmail.com>
2026-03-20 13:40:20 -0600

    still not working

--- Test.py
+++ Test.py
@@ -345,24 +345,47 @@
 
 
 
-class Human:
-    def __init__(self, name, age, email):
-        self.name = name
-        self.age = age
-        self.email = email
-    def get_name(self):
-        return self.name
-    def get_age(self):
-        return self.age
-    def get_email(self):
-        return self.email
-    def print_info(self):
-        print(f"Name = {jeremy.name}\nAge = {jeremy.age}\nEmail = {jeremy.email}")
+# class Human:
+#     def __init__(self, name, age, email):
+#         self.name = name
+#         self.age = age
+#         self.email = email
+#     def get_name(self):
+#         return self.name
+#     def get_age(self):
+#         return self.age
+#     def get_email(self):
+#         return self.email
+#     def print_info(self):
+#         print(f"Name = {jeremy.name}\nAge = {jeremy.age}\nEmail = {jeremy.email}")
+#
+# jeremy = Human("Jeremy Wlasitz", 21, "jeremy.wlasitz@edu.sait.ca")
+# print(f"{jeremy.get_name()}\n"
+#       f"{jeremy.get_age()}\n"
+#       f"{jeremy.get_email()}\n")
+# print(jeremy.print_info())
 
-jeremy = Human("Jeremy Wlasitz", 21, "jeremy.wlasitz@edu.sait.ca")
 
-print(f"{jeremy.get_name()}\n"
-      f"{jeremy.get_age()}\n"
-      f"{jeremy.get_email()}\n")
 
-print(jeremy.print_info())
+
+class Rectangle:
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+    def get_width(self):
+        return self.width
+    def get_length(self):
+        return self.length
+    def set_width(self, new_width):
+        self.width = new_width
+    def set_length(self, new_length):
+        self.length = new_length
+    def perimeter(self):
+        return int(self.width) * 2 + int(self.length) * 2
+    def area(self):
+        return int(self.width) * int(self.length)
+width = input("Enter the width:\n")
+length = input("Enter the height:\n")
+x = Rectangle(width, length)
+print(x.perimeter())
+print(x.area())
