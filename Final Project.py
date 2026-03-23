3421343975502cc1167320ddcce2ead0b8bf94d4
wlasitz-jeremy <wlasitz@gmail.com>
2026-03-22 20:17:01 -0600

    initial commit

---
+++ Final Project.py
@@ -0,0 +1,54 @@
+# Import OS Module
+import os
+
+# Constants
+VALID_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+VALID_HOURS = 9-17
+ROOMS = (101, 102, 201)
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    print(f"Simple Hotel Booking\n"
+        f"1) Add booking\n"
+        f"2) Show day calender"
+        f"3) Find booking by guest"
+        f"4) Cancel booking"
+        f"5) Change booking"
+        f"6) Exit")
+    option = input("Select option: ")
+
+
+
+    # while option != '6':
+    #
+    #     if option == '1':
+    #
+    #
+    #     elif option == '2':
+    #
+    #
+    #     elif option == '3':
+    #
+    #
+    #     elif option == '4':
+    #
+    #
+    #     elif option == '5':
+    #
+    #
+    #     else:
+    #         print("Invalid choice, please enter number between 1 and 6")
+    #     option = input("Enter number between 1 and 5: ").strip()
+    # print ("Saved. Goodbye.")
